ce005fb9b49bb418839eae37bd76a5efcae76168
ecourse <smkang@codenuri.co.kr>
2022-01-24 10:34:34 +0900

    Add files via upload

--- 1_namespace3.cpp
+++ 1_namespace3.cpp
@@ -1,7 +1,21 @@
 // 9 page
 #include <algorithm>
+using namespace std;
+
+int count = 10;
 
 int main()
 {
-	int n = min(10, 30); // C++ 표준 함수. <alogrithm>헤더에 있습니다.
+	// 모든 C++ 표준은 "std"라는 이름공간 안에 있습니다.
+	int n = std::min(10, 30); // C++ 표준 함수. <alogrithm>헤더에 있습니다.
+
+	int n2 = count; // 왜 에러일까요 ?
+					// C++ 표준에 "std::count()" 라는 함수 가있습니다.
+					// using namespace std 때문에
+					// 전역변수 count 와 이름 충돌
 }
+
+
+
+
+
